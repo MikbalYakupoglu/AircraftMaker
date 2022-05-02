62c6c046aa0e012a2ec44c9ec05c6c9a50833764
Muhammet İkbal Yakupoglu <ikbal.yakupoglu@ogr.sakarya.edu.tr>
2022-05-02 22:15:32 +0300

    .

--- AircraftMaker/Properties/AssemblyInfo.cs
+++ AircraftMaker/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("B211200300_FormGameProject")]
+[assembly: AssemblyTitle("AircraftMaker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("B211200300_FormGameProject")]
+[assembly: AssemblyProduct("AircraftMaker")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
