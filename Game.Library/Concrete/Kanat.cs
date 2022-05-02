aa95c6b20113896a702f19d1f551693eba63ae4e
Muhammet İkbal Yakupoglu <ikbal.yakupoglu@ogr.sakarya.edu.tr>
2022-05-02 03:03:36 +0300

    Oyun Bitimi AnaMenuye Donme Bugu Fixlendi

---
+++ Game.Library/Concrete/Kanat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Library.Abstract;
+
+namespace Game.Library.Concrete
+{
+    internal class Kanat : ToplananCisim
+    {
+        public Kanat(int panelUzunlugu, int panelGenisligi) : base(panelUzunlugu, panelGenisligi)
+        {
+        }
+    }
+}
---
+++ Game.Library/Concrete/Kod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Library.Abstract;
+
+namespace Game.Library.Concrete
+{
+    internal class Kod : ToplananCisim
+    {
+        public Kod(int panelUzunlugu, int panelGenisligi) : base(panelUzunlugu, panelGenisligi)
+        {
+        }
+    }
+}
